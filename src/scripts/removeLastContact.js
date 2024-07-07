76f9862ee8322565067dc13190a097d15391e96b
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-07-07 15:54:47 +0300

    add removeLastContact func

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,18 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  const data = await fs.readFile(PATH_DB, { encoding: 'utf8' });
+  const contacts = JSON.parse(data);
 
-removeLastContact();
+  if (contacts.length > 0) {
+    contacts.pop();
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, undefined, 2));
+  } else {
+    console.log('No contacts to remove.');
+  }
+};
+
+removeLastContact()
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
