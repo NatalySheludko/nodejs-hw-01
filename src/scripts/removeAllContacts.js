df6c4b0fa22dd934f4bdbb852fc865accd84b36a
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-07-07 15:55:06 +0300

    add removeAllContacts func

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,11 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  const data = await fs.writeFile(PATH_DB, JSON.stringify([], undefined, 2));
+  return data;
+};
 
-removeAllContacts();
+removeAllContacts()
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
