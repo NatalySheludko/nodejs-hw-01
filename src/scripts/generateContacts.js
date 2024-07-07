f6f894bf85ca77407886c5b3037859440f2e978e
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-07-07 15:55:33 +0300

    add generateContacts func

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,16 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  const data = await fs.readFile(PATH_DB, { encoding: 'utf8' });
+  const contacts = JSON.parse(data);
 
-generateContacts(5);
+  for (let i = 0; i < number; i += 1) {
+    contacts.push(createFakeContact());
+  }
+
+  await fs.writeFile(PATH_DB, JSON.stringify(contacts, undefined, 2));
+};
+
+generateContacts(5).catch((error) => console.error(error));
