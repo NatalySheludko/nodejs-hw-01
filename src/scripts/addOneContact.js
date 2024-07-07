4f6becd226566ba0fb0c3a1fe96dac74bacec566
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-07-07 15:56:01 +0300

    add addOneContact func

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,14 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  const data = await fs.readFile(PATH_DB, { encoding: 'utf8' });
+  const contacts = JSON.parse(data);
 
-addOneContact();
+  contacts.push(createFakeContact());
+
+  await fs.writeFile(PATH_DB, JSON.stringify(contacts, undefined, 2));
+};
+
+addOneContact().catch((error) => console.error(error));
